cb5efc61e3db407db4d561730f9a1b8bdfbf3b12
ImamFerdian <200705073@student.ar-raniry.ac.id>
2023-06-18 00:00:31 +0700

    commit pertama

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+class Home extends Component {
+ render() {
+ return (
+ <div>
+ <h2>Home</h2>
+ <p>Ini merupakan aplikasi website Single Page Application yang dibangun dengan menggunakan Framework reactJS. Aplikasi web SPA ini dipelajari pada mata kuliah SISTEM WEB DAN MOBILE program studi TEKNOLOGI INFORMASI-SAINTEK UIN Ar-Raniry Banda Aceh.</p>
+ </div> 
+ );
+ }
+}
+export default Home;
